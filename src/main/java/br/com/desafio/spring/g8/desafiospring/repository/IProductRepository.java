32b0ee621de0fdaea396083231b0779a974027da
vfeitoza <vinicius.feitoza@mercadolivre.com>
2021-12-29 12:35:38 -0300

    importa - ioexception

--- src/main/java/br/com/desafio/spring/g8/desafiospring/repository/IProductRepository.java
+++ src/main/java/br/com/desafio/spring/g8/desafiospring/repository/IProductRepository.java
@@ -1,11 +1,12 @@
 package br.com.desafio.spring.g8.desafiospring.repository;
 
+import java.io.IOException;
 import java.util.List;
 
 public interface IProductRepository<T> {
-        void save(T t);
-        List<T> findAllAvailableProduct();
-        List<T> findAllCategoryProduct(String categoryName);
+        void save(T t) throws IOException;
+        List<T> findAllAvailableProduct() throws IOException;
+        List<T> findAllCategoryProduct(String categoryName) throws IOException;
         List<T> findAllProductTwoCategory(String p1,String p2);
         List<T> findAllProductDecreasingAlfa();
         List<T> findAllProductIncreasingAlfa();
