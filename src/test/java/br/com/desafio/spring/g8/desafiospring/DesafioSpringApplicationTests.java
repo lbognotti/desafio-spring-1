c5af465fb5bbbd88fda964931f6c261adbf25aca
lbognotti <lucas.bognotti@mercadolivre.com>
2021-12-29 09:47:12 -0300

    first commit

---
+++ src/test/java/br/com/desafio/spring/g8/desafiospring/DesafioSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.desafio.spring.g8.desafiospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
