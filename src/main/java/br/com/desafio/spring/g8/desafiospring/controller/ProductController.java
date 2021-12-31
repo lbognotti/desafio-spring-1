cf9068361b3c72d3b7a7f6e053fd838d057bd0ad
Ronaldd Pinho <ronaldd.pinho@mercadolivre.com>
2021-12-31 12:09:55 -0300

    refatora - move conversão de listas DTO-Entidade para métodos específicos

--- src/main/java/br/com/desafio/spring/g8/desafiospring/controller/ProductController.java
+++ src/main/java/br/com/desafio/spring/g8/desafiospring/controller/ProductController.java
@@ -1,5 +1,6 @@
 package br.com.desafio.spring.g8.desafiospring.controller;
 
+import br.com.desafio.spring.g8.desafiospring.dto.ArrayMapper;
 import br.com.desafio.spring.g8.desafiospring.dto.CreateProductsDTO;
 import br.com.desafio.spring.g8.desafiospring.dto.ListProductsDTO;
 import br.com.desafio.spring.g8.desafiospring.entity.Product;
@@ -11,7 +12,6 @@ import org.springframework.web.bind.annotation.*;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 @RestController()
 @RequestMapping("/api/v1")
@@ -22,48 +22,16 @@ public class ProductController {
 
     @PostMapping("/insert-articles-request")
     public ResponseEntity<ListProductsDTO> create(@RequestBody CreateProductsDTO createProductsDTO) throws IOException {
-        List<Product> products = createProductsDTO.getArticles()
-                .stream()
-                .map(article -> article.toEntity())
-                .collect(Collectors.toList());
-
+        List<Product> products = ArrayMapper.dtosToProducts(createProductsDTO.getArticles());
         this.productService.createProduct(products);
-
-        ListProductsDTO response = ListProductsDTO.builder()
+        return ResponseEntity.ok(ListProductsDTO
+                .builder()
                 .articlesDTO(createProductsDTO.getArticles())
-                .build();
-        return ResponseEntity.ok(response);
+                .build());
     }
 
     @GetMapping("/articles")
     public ResponseEntity<List<Product>> findAllProductFilter(@RequestParam Map<String, String> allParams) throws IOException {
         return ResponseEntity.ok(this.productService.findFilter(allParams));
     }
-
-//    @GetMapping("/findAllDto")
-//    public ResponseEntity<ListProductsDTO> findAllDto() throws IOException {
-//        List<ProductDTO> productsDto = this.productService.findAllProduct()
-//                .stream()
-//                .map(product -> ProductDTO.fromEntity(product))
-//                .collect(Collectors.toList());
-//
-//        ListProductsDTO responseBody = ListProductsDTO.builder()
-//                .articlesDTO(productsDto)
-//                .build();
-//
-//        return ResponseEntity.ok(responseBody);
-//    }
-//
-//    //Teste de DTO - Post
-//    @PostMapping("/createDto")
-//    public ResponseEntity<Object> createDto(@RequestBody CreateProductsDTO createProductsDTO) throws IOException {
-//        List<Product> products = createProductsDTO.getArticles()
-//                .stream()
-//                .map(article -> article.toEntity())
-//                .collect(Collectors.toList());
-//
-//        this.productService.createProduct(products);
-//        return ResponseEntity.ok().build();
-//    }
-//    }
 }
