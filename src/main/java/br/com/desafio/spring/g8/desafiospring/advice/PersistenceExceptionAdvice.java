4a076fc24e71df8910e56f7b8da802ce11ac9df4
Ronaldd Pinho <ronaldd.pinho@mercadolivre.com>
2022-01-03 12:58:04 -0300

    adiciona - comentários JavaDoc nas classes de Advice e Handlers

--- src/main/java/br/com/desafio/spring/g8/desafiospring/advice/PersistenceExceptionAdvice.java
+++ src/main/java/br/com/desafio/spring/g8/desafiospring/advice/PersistenceExceptionAdvice.java
@@ -8,15 +8,33 @@ import org.springframework.web.context.request.WebRequest;
 
 import java.io.IOException;
 
+/**
+ * @author Lucas Bognotti
+ * @description Classe de Advice para tratamento de exceções nas camadas do sistema.
+ */
 @ControllerAdvice
 public class PersistenceExceptionAdvice {
 
+    /**
+     * @author Lucas Bognotti
+     * @description Captura exceções de IOException e retorna BAD REQUEST (400).
+     * @param exception a exceção capturada
+     * @param request contexto da requisição
+     * @return Resposta da requisição com um Bad Request
+     */
     @ExceptionHandler(value = IOException.class)
     protected ResponseEntity<Object> handleIOExpection(IOException exception, WebRequest request) {
        String bodyOfResponse = exception.getMessage();
        return ResponseEntity.badRequest().body(bodyOfResponse);
     }
 
+    /**
+     * @author Lucas Bognotti
+     * @description Captura exceções de ProductNotFoundException e retorna NOT FOUND (404).
+     * @param exception a exceção capturada
+     * @param request contexto da requisição
+     * @return Resposta da requisição com um Not Found
+     */
     @ExceptionHandler(value = ProductNotFoundException.class)
     protected ResponseEntity<Object> handleProductNotFoundExpection(ProductNotFoundException exception, WebRequest request) {
         return ResponseEntity.notFound().build();
--- src/main/java/br/com/desafio/spring/g8/desafiospring/advice/handler/ProductNotFoundException.java
+++ src/main/java/br/com/desafio/spring/g8/desafiospring/advice/handler/ProductNotFoundException.java
@@ -1,5 +1,9 @@
 package br.com.desafio.spring.g8.desafiospring.advice.handler;
 
+/**
+ * @author Lucas Bognotti
+ * @description Exceção criada para ser lançada quando um produto não for encontrada na base de dados (JSON).
+ */
 public class ProductNotFoundException extends RuntimeException {
     static final long serialVersionUID = -7034897190745766939L;
 
